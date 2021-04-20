1edb492d519aea93882b5d96e872ebfc8a094f6e
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-21 00:56:43 +0600

    Creating custom user model

--- BurgerApi/admin.py
+++ BurgerApi/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from BurgerApi.models import UserProfile
 
 # Register your models here.
+
+admin.site.register(UserProfile)
--- BurgerApi/models.py
+++ BurgerApi/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+# Create your models
+
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('User Must Have Email!')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
