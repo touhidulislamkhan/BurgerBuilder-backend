12ba4365c11a2e2bb57e8a86f673afa05780c9d7
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-21 03:18:01 +0600

    adding jwt authentication

--- BurgerApi/urls.py
+++ BurgerApi/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewset
 
@@ -6,5 +11,6 @@ router = DefaultRouter()
 router.register(r'user', UserProfileViewset)
 
 urlpatterns = [
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
--- MyRestApi/settings.py
+++ MyRestApi/settings.py
@@ -55,6 +55,12 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 ROOT_URLCONF = 'MyRestApi.urls'
 
 TEMPLATES = [
