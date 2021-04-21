946acdd0dd241c3702fbd1ba7a19d74bf98ad505
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-21 23:48:22 +0600

    Creating order model

--- BurgerApi/admin.py
+++ BurgerApi/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from BurgerApi.models import UserProfile
+from BurgerApi.models import UserProfile, Order, CustomerDetail, Ingredient
 
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Order)
+admin.site.register(CustomerDetail)
+admin.site.register(Ingredient)
--- BurgerApi/models.py
+++ BurgerApi/models.py
@@ -40,3 +40,34 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Ingredient(models.Model):
+    salad = models.IntegerField(default=0)
+    cheese = models.IntegerField(default=0)
+    meat = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.salad + self.cheese + self.meat
+
+
+class CustomerDetail(models.Model):
+    deliveryAddress = models.TextField(blank=True)
+    phone = models.CharField(max_length=255, blank=True)
+    paymentType = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.deliveryAddress
+
+
+class Order(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    ingredients = models.OneToOneField(
+        Ingredient, on_delete=models.CASCADE, null=True)
+    customer = models.OneToOneField(
+        CustomerDetail, on_delete=models.CASCADE, null=True)
+    price = models.CharField(max_length=20, blank=False)
+    orderTime = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.user + 's Orders'
